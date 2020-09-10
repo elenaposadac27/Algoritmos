4430958507c3f7817386b5e44439a1515ad32354
elenaposadac27 <posada.elena@uces.edu.co>
2020-09-10 14:03:59 -0500

    subiendo taller programacion funcional

--- TALLERES/taller5/taller_programacion_funcional.py
+++ TALLERES/taller5/taller_programacion_funcional.py
@@ -22,9 +22,37 @@ elevado = lambda elemento :elemento**2
 listaCuadrados = list(map(elevado,lista))
 print(listaCuadrados)
 
+#punto2
+dividir = lambda elemento : elemento/max(lista)
+listaCocientes = list(map(dividir,lista))
+print(listaCocientes)
+
+#punto3
+numero = int(input("ingrese el valor que desea restar"))
+restar = lambda elemento : elemento - numero
+listaDiferencias= list(map(restar,lista))
+print(listaDiferencias)
+
 #FUNCIONES FILTER
 #punto1
 listica= [3,21,58,63,14,2]
 div_7 = lambda elemento : elemento %7 == 0
 lista_div_7 = list (filter(div_7,listica))
 print (lista_div_7)
+
+#punto2
+listaEstudiantes=["Juan", "Sofia", "Nicolas", "Sebastian", "Maria Clara", "Ana"]
+largoMenor5 = lambda nombre : len(nombre) < 5
+lista_nombres_menores_5 = list(filter(largoMenor5,listaEstudiantes))
+print(lista_nombres_menores_5)
+
+#punto3
+Numeros=[5,2,8,20,6,10,53,85,32,14,9]
+par = lambda elemento : elemento %2 == 0
+listaPares = list (filter(par,Numeros))
+print (listaPares)
+
+#punto4
+impar = lambda elemento : elemento %2 != 0
+listaImpares = list (filter(impar,Numeros))
+print (listaImpares)
