c3e9a8babc89a7a97607b4b778e0774dde956913
elenaposadac27 <posada.elena@uces.edu.co>
2020-11-04 15:19:19 -0500

    subiendo taller pyflask

--- TALLERES/taller_pyflask/main_taller.py
+++ TALLERES/taller_pyflask/main_taller.py
@@ -5,35 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def homeRoute():
-    response = make_response(redirect("home"))
-    response.set_cookie("place1", "Dubrovnik, Croacia")
-    response.set_cookie("place2", "Orlando, Estados Unidos")
-    response.set_cookie("place3", "Paris, Francia")
-    response.set_cookie("place4", "Madrid, España")
-    response.set_cookie("place5", "Medellin, Colombia")
-    response.set_cookie("person1", "Mama")
-    response.set_cookie("person2", "Papa")
-    response.set_cookie("person3", "Hermanas")
-    response.set_cookie("person4", "Abuelos")
-    response.set_cookie("person5", "Nicolas")
     return render_template("home.html")
 
 @app.route("/places")
 def placesRoute():
-    place1 = request.cookies.get("place1")
-    place2 = request.cookies.get("place2")
-    place3 = request.cookies.get("place3")
-    place4 = request.cookies.get("place4")
-    place5 = request.cookies.get("place5")
-    return render_template("places.html", lugar1 = place1, lugar2 = place2, lugar3 = place3, lugar4 = place4, lugar5 = place5)
-
+    return render_template("places.html")
 @app.route("/people")
 def peopleRoute():
-    person1 = request.cookies.get("person1")
-    person2 = request.cookies.get("person2")
-    person3 = request.cookies.get("person3")
-    person4 = request.cookies.get("person4")
-    person5 = request.cookies.get("person5")
-    return render_template("people.html", persona1 = person1, persona2 = person2, persona3 = person3, persona4 = person4, persona5 = person5)
+    return render_template("people.html")
 if __name__== "__main__":
     app.run()
